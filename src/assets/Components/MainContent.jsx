716f50621ab2230ef583fa640577962b788ef81a
Med Aziz Laafine <laafine.med.aziz@gmail.com>
2024-08-13 00:10:25 +0100

    commit 2

--- src/assets/Components/MainContent.jsx
+++ src/assets/Components/MainContent.jsx
@@ -3,6 +3,12 @@ export function MainContent(){
     return(
         <div className="Container">
             <img src={MyImage} alt="My image" width="250rem" className="Me--Image"/>
+            <h1>Med Aziz Laafine</h1>
+            <ul className="coordinates">
+                <li className="Adress">19,9 Avril,Ennajet,Oued Ellil,Chabaw,Manouba,2021</li>
+                <li className="Email">laafine.med.aziz@gmail.com</li>
+                <li className="Phone">+216 24335292</li>
+            </ul>
         </div>
     )
 }
